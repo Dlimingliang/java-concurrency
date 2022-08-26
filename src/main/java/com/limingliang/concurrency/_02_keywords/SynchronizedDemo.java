e4da44ff137631418aa571a4478653504e986b3f
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-08-26 16:20:15 +0800

    thread

---
+++ src/main/java/com/limingliang/concurrency/_02_keywords/SynchronizedDemo.java
@@ -0,0 +1,30 @@
+package com.limingliang.concurrency._02_keywords;
+
+public class SynchronizedDemo implements Runnable {
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new SynchronizedDemo());
+            thread.start();
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("result: " + count);
+    }
+
+    @Override
+    public void run() {
+//        for (int i = 0; i < 100000; i++) {
+//            count++;
+//        }
+        synchronized (SynchronizedDemo.class) {
+            for (int i = 0; i < 100000; i++) {
+                count++;
+            }
+        }
+    }
+}
